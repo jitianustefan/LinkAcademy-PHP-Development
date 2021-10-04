6cee2977dc9c359af6d5aceb70ca70f1eb9868b0
jitianustefan <44315455+jitianustefan@users.noreply.github.com>
2021-10-04 12:47:38 +0300

    Add files via upload

---
+++ 2.Dreamweaver/mydream2021/div.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Proiect nou</title>
+	<?php include("html/head.html"); ?>
+</head>
+<body>
+<container class="wrapper">
+	<?php include("html/header.php");?>
+	<?php include("html/div.html"); ?>
+	<?php include("html/footer.html");?>
+</container>	
+</body>
+</html>
